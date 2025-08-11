8fd2cd65bb07f41158fb0f0418087b427f389f0b
Jake Norton <jake.norton88@gmail.com>
2025-08-11 16:14:57 +1200

    add training recipes and train_all for natgen

--- recipes/graphcodebert.sh
+++ recipes/graphcodebert.sh
@@ -1,15 +1,17 @@
-export model_name=../models/pretrained/natgen
-export tokenizer_name=Salesforce/codet5-base
+# Slurm batch recipe for graphcodebert
+export model_name=microsoft/graphcodebert-base
+export tokenizer_name=microsoft/graphcodebert-base
 export pos_weight=1.0
 export out_suffix=default
 export epoch=10
-export model_type=natgen
+export model_type=roberta
 
 echo "Submitted jobs with model_name: $model_name model_type: $model_type pos: $pos_weight out_suffix $out_suffix epoch: $epoch tokenizer_name: $tokenizer_name"
 
 sbatch_args() {
     local job_name=$1
-    echo "--gpus-per-node=1 --partition=aoraki_gpu_H100 --mem=64gb --job-name=${job_name}_${out_suffix} --time=70:00:00 --output=/projects/sciences/computing/norja159/research/scripts/${job_name}_out/${model_type}_%j.out"
+    echo "--gpus-per-node=1 --partition=aoraki_gpu_H100 --mem=64gb --job-name=${job_name}_${out_suffix} --time=70:00:00 --output=/projects/sciences/computing/norja159/research/scripts/${job_name}_out/%j.out"
+    #echo "--gpus-per-node=1 --partition=aoraki_gpu --mem=64gb --job-name=$job_name --time=70:00:00 --output=/projects/sciences/computing/norja159/research/scripts/${job_name}_out/%j.out"
 }
 
 export dir=icvul
