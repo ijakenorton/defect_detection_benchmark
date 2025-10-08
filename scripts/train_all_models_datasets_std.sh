abb69f9b3b3224c0066a2247657d8d686dbc8570
Jake Norton <jake.norton88@gmail.com>
2025-10-08 18:22:08 +1300

    fix env var name

--- scripts/train_all_models_datasets_std.sh
+++ scripts/train_all_models_datasets_std.sh
@@ -13,14 +13,13 @@ setup_paths
 export out_suffix=splits
 export pos_weight=1.0
 export epoch=5
-export model_config_dir="./model_configs"
 seeds=(123456 789012 345678)
 datasets=(icvul mvdsc_mixed devign vuldeepecker cvefixes juliet reveal)
 big_datasets=(diversevul draper) 
 
 #Run
 echo "=========================Config==========================="
-ls -1 ${model_config_dir}/*.sh
+ls -1 ${MODEL_CONFIG_DIR}/*.sh
 echo seeds: "${seeds[@]}"
 echo datasets: "${datasets[@]}"
 echo big_datasets: "${big_datasets[@]}"
